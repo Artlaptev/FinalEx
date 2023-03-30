d7ccf30ac17730688abde4fa9a2bc5c334f5dd42
Artlaptev <89065927+Artlaptev@users.noreply.github.com>
2023-03-30 22:34:39 +0300

    created project

---
+++ FinalEx/FinalEx/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalEx
+{
+    public enum AnimalType 
+    {
+        cat,dog,camel,horse,unicorn
+
+    }
+    public class Animal
+    {
+        public string Name { get; private  set; }
+        public AnimalType Type { get; private set; }
+        public DateOnly Birthday { get; private set; }
+        public Dictionary<string,string> Commands { get; private set; }
+        
+        public Animal (string name, AnimalType type, DateOnly birthday)
+        {
+            Name = name;
+            Type = type;
+            Birthday = birthday;
+        }
+        public string DoCommand(string command)
+        {
+            if (Commands == null) return "I can't do anithing";
+            if (Commands.ContainsKey(command))
+                return Commands[command];
+            else return @"I don't know this command \,/(^_^)\,/";
+
+        }
+        public bool LearnCommand(string command, string doing)
+        {
+            if (Commands==null)
+                Commands = new Dictionary<string,string>();
+            if (Commands.ContainsKey(command))
+                return false;
+            Commands.Add(command, doing);
+            return true;
+        }
+
+        
+    }
+}
---
+++ FinalEx/FinalEx/AnimalHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalEx
+{
+    public  class AnimalHandler
+    {
+        public Hashtable Animals { get; private set; } 
+        public void AddAnimal(string name, AnimalType type,DateOnly birhtday)
+        {
+            if (Animals==null)
+                Animals= new Hashtable();
+            Animals.Add(name,new Animal(name, type, birhtday));
+        }
+        public bool LearnTheCommand(string animalName, string commandDescription, string commandDoung)
+        {
+            if (!Animals.ContainsKey(animalName))
+                return false;
+            Animal animal = Animals[animalName] as Animal;
+            animal.LearnCommand(commandDescription, commandDoung);
+            
+            return true;
+        }  
+    }
+}
